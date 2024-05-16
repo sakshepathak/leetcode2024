843eaadff0a856552fc1344df6732e10f7e1b0d6
Sakshi Pathak <146483225+sakshepathak@users.noreply.github.com>
2024-05-17 03:48:13 +0530

    Create evalTree.java
    ou are given the root of a full binary tree with the following properties:
    
    Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
    Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
    The evaluation of a node is as follows:
    
    If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
    Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
    Return the boolean result of evaluating the root node.
    
    A full binary tree is a binary tree where each node has either 0 or 2 children.
    
    A leaf node is a node that has zero children.

---
+++ evalTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.left == null && root.right ==null){
+            return root.val ==1;
+        }
+        boolean evalLeft = evaluateTree(root.left);
+        boolean evalRight = evaluateTree(root.right);
+        boolean evalRoot;
+        if(root.val == 2) {
+            evalRoot = evalLeft | evalRight;
+        }else{
+            evalRoot = evalLeft & evalRight;
+        }
+        return evalRoot;
+    }
+}
